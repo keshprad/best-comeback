f14bfddb333226ee5a03bf0106855b0bfc9f0002
keshprad <keshav.r.pradeep@gmail.com>
2021-02-01 16:00:02 -0800

    resizing text, pasting props on person, making gif

--- main.py
+++ main.py
@@ -14,14 +14,13 @@ path = terminal_parser.parse_args().i
 img = Image.open(path)
 # Open assets
 sunglass = Image.open('assets/sunglasses.png')
-smoke = Image.open('assets/smoke.png')
+cig = Image.open('assets/cig.png')
 text = Image.open('assets/text.png')
 
 # Resize image if too big
 max_width = 750
-height, width = img.size
-if width > max_width:
-    scaled_height = int(max_width * height / width)
+if img.width > max_width:
+    scaled_height = int(max_width * img.height / img.width)
     img.thumbnail((max_width, scaled_height))
 
 # Convert to grayscale and represent as numpy array
@@ -63,19 +62,50 @@ for rect in rects:
     # Find angle between eye center points
     angle = np.rad2deg(np.arctan2(leftEyeCenter[1] - rightEyeCenter[1], leftEyeCenter[0] - rightEyeCenter[0]))
 
-    # Edit sunglasses to fit face
+    # Edit props to fit
     # Resizing and downsampling with LANCZOS
-    current_sunglass = sunglass.resize((sunglass_width, int(sunglass_width * sunglass.size[1] / sunglass.size[0])),
+    current_sunglass = sunglass.resize((sunglass_width, int(sunglass_width * sunglass.height / sunglass.width)),
                                        resample=Image.LANCZOS)
+    # current_cig = cig.resize((cig))
+
     # Rotate to angle of eyes
     current_sunglass = current_sunglass.rotate(angle, expand=True)
     current_sunglass = current_sunglass.transpose(Image.FLIP_TOP_BOTTOM)
 
     # Line up props with image and add to faces
     face['sunglasses'] = current_sunglass
-    leftEye_x = leftEye[0, 0] - sunglass_width // 4
-    leftEye_y = leftEye[0, 1] - sunglass_width // 6
-    face['sunglass_position'] = (leftEye_x, leftEye_y)
+    leftEye_x = int(leftEye[0, 0] - sunglass_width // 4)
+    leftEye_y = int(leftEye[0, 1] - sunglass_width // 6)
+    face['sunglass_pos'] = (leftEye_x, leftEye_y)
     faces.append(face)
 
-# if __name__ == "__main__":
+duration, text_duration = 5, 2
+if img.width > img.height:
+    text_width = int(img.width / 2)
+else:
+    text_width = int(3 * img.width / 4)
+text_height = int(text_width * text.height / text.width)
+text = text.resize((text_width, text_height), resample=Image.LANCZOS).convert('RGBA')
+
+
+def make_frame(t):
+    out_img = img.convert('RGBA')
+
+    if t == 0:  # no glasses
+        return np.asarray(out_img)
+
+    for face in faces:
+        if t <= duration - text_duration:  # last secs for text
+            curr_x = face['sunglass_pos'][0]  # x is constant
+            curr_y = int(face['sunglass_pos'][1] * t / (duration - text_duration))  # y moves from top -> down
+            out_img.paste(face['sunglasses'], (curr_x, curr_y), face['sunglasses'])
+            out_img.show()
+        else:  # draw the text
+            out_img.paste(face['sunglasses'], face['sunglass_pos'], face['sunglasses'])
+            text_pos = (out_img.width // 2 - text.width // 2, out_img.height - text.height)
+            out_img.paste(text, text_pos, text)
+    return np.asarray(out_img)
+
+
+animation = mpy.VideoClip(make_frame, duration=duration)
+animation.write_gif("DealWithIt.gif", fps=4)
